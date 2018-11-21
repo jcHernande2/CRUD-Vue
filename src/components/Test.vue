ba477cd1c53f6a420171405901bace8afdb78bed
carlos <hhjuan27@gmail.com>
2018-11-21 17:12:58 -0600

    actualizar

---
+++ src/components/Test.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div v-text="title"></div>
+        <div v-html="myHtml"></div>
+        <div v-if="showName">
+            <p>{{user.firstName}}</p>
+        </div>
+        <div v-else="showName">
+                <p>no puedo mostrar nombre</p>
+        </div>
+        <hr>
+        <ul >
+            <li v-for="task in tasks">
+                {{task.title}}
+            </li>
+           
+        </ul>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                title: 'Hola Vuejs',
+                user:{
+                    firstName:'joe',
+                    lastName:'McMillian'
+                }
+                ,myHtml:'<h3>Hola vue</h3>'
+                ,showName:false
+                ,tasks:[
+                    {title:'eat'}
+                    ,{title:'dinner'}
+                    ,{title:'play'}
+                ]
+            }
+        }
+    }
+</script>
+<style>
+</style>
